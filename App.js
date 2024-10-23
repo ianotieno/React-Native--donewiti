b8596ead9f230d600fa90113b7a7c81f9fa19f17
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-10-23 18:24:55 +0300

    update

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
+import { StyleSheet, Text, Image,SafeAreaView } from 'react-native';
 
 export default function App() {
-  const handlePress= ()=>console.log("text clicked")
   return (
     <SafeAreaView style={styles.container}>
-      <Text >
-      You need to enable JavaScript to run this app. 
-    </Text>
-      <StatusBar style="auto" />
+      <Text>Open up App.js to start working on your app!</Text>
+   <Image  source={{
+    
+    uri:"https://picsum.photos/id/237/200/300"}}/>
     </SafeAreaView>
   );
 }
@@ -17,8 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: "center",
-    alignItems:"center"
-    
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
